c0125669c9f3bc4c599cd9f4573ea616e5a8dbc8
Himanshi Sharma <himanshish456@gmail.com>
2024-08-22 16:10:50 +0530

    removed unnecessary code for password generator

--- src/main/java/com/example/user_management/config/SpringConfig.java
+++ src/main/java/com/example/user_management/config/SpringConfig.java
@@ -15,14 +15,10 @@ public class SpringConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(customizer -> customizer.disable())
-                .authorizeHttpRequests(request -> request
-                        .requestMatchers("register")
-                        .permitAll()
-                        .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
+
+        http
+                .csrf(customizer -> customizer.disable());
+        return http.build();
+
     }
 }
--- src/main/java/com/example/user_management/model/User.java
+++ src/main/java/com/example/user_management/model/User.java
@@ -30,7 +30,7 @@ public class User {
     private String password;
 
     public User(){
-        
+
     }
 
 
